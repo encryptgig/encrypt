fb8b60f379f4f1401e7bc8ccc66393c0be1ee848
Rajni Kant <rajni@ves.io>
2020-11-14 17:40:32 +0530

    support all based on backend

--- script.js
+++ script.js
@@ -135,7 +135,7 @@ function startApp() {
         // Retrieve the singleton for the GoogleAuth library and set up the client.
         auth2 = gapi.auth2.init({
             //client_id: git_client,
-            client_id: local_client,
+            client_id: git_client,
             cookiepolicy: 'single_host_origin',
             // Request scopes in addition to 'profile' and 'email'
             //scope: 'additional_scope'
